fd7b04c88da5dc9765476c7fd11fa5dceca42303
Miftahul Arifin <miftahul97@gmail.com>
2023-11-28 07:07:08 +0700

    feat: add msg

---
+++ src/msg.rs
@@ -0,0 +1,27 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {
+    pub admins: Vec<String>,
+    pub members: Vec<String>,
+    pub mutable: bool,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    // Freeze will make a mutable contact immutable, must be called by an admin
+    Freeze {},
+    // UpdateAdmins will change current admins, must be called by an admin
+    UpdateAdmins { admins: Vec<String> },
+    // UpdateMembers will change current members, must be called by an admin
+    UpdateMembers { members: Vec<String> },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    AdminList {},
+    Memberlist {},
+}
