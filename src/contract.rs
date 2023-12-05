9c1351d0d50c2b0ba5d517e9662ddc2ce15eb78c
Miftahul Arifin <miftahul97@gmail.com>
2023-12-06 05:48:37 +0700

    feat: add and remove members

--- src/contract.rs
+++ src/contract.rs
@@ -41,8 +41,8 @@ pub fn execute(
         ExecuteMsg::Freeze {  } => Ok(Response::new()),
         ExecuteMsg::AddAdmins { admins } => exec::add_admins(deps, info, admins),
         ExecuteMsg::RemoveAdmins { admins } => exec::remove_admins(deps, info, admins),
-        ExecuteMsg::AddMembers { members } => Ok(Response::new()),
-        ExecuteMsg::RemoveMembers { members } => Ok(Response::new()),
+        ExecuteMsg::AddMembers { members } => exec::add_members(deps, info, members),
+        ExecuteMsg::RemoveMembers { members } => exec::remove_members(deps, info, members),
     }
 }
 
@@ -84,6 +84,43 @@ mod exec {
         STATE.save(deps.storage, &curr_state)?;
 
         Ok(Response::new().add_attribute("action", "remove_admins"))
+    }
+
+    pub fn add_members(
+        deps: DepsMut,
+        info: MessageInfo,
+        members: Vec<String>,
+    ) -> Result<Response, ContractError> {
+        let mut curr_state = STATE.load(deps.storage)?;
+        if !curr_state.can_modify(info.sender.as_ref()) {
+            return Err(ContractError::Unauthorized {
+                sender: info.sender,
+            });
+        }
+        
+        let mut members = map_validate(deps.api, &members)?;
+        curr_state.members.append(&mut members);
+        STATE.save(deps.storage, &curr_state)?;
+
+        Ok(Response::new().add_attribute("action", "add_members"))
+    }
+
+    pub fn remove_members (
+        deps: DepsMut,
+        info: MessageInfo,
+        members: Vec<String>,
+    ) -> Result<Response, ContractError> {
+        let mut curr_state = STATE.load(deps.storage)?;
+        if !curr_state.can_modify(info.sender.as_ref()) {
+            return Err(ContractError::Unauthorized {
+                sender: info.sender,
+            });
+        }
+        let members = map_validate(deps.api, &members)?;
+        curr_state.members.retain(|curr_member| !members.contains(curr_member));
+        STATE.save(deps.storage, &curr_state)?;
+
+        Ok(Response::new().add_attribute("action", "remove_members"))
     }
 }
 
@@ -310,6 +347,118 @@ mod tests {
             resp,
             AdminListResponse {
                 admins: vec![ALICE.to_string(), Addr::unchecked("owner").to_string()],
+            }
+        );
+    }
+
+    #[test]
+    fn exec_add_members() {
+        let mut app = App::default();
+
+        let code = ContractWrapper::new(execute, instantiate, query);
+        let code_id = app.store_code(Box::new(code));
+
+        let addr = app
+            .instantiate_contract(
+                code_id,
+                Addr::unchecked("owner"),
+                &InstantiateMsg {
+                    admins: vec![Addr::unchecked("owner").to_string()],
+                    members: vec![ALICE.to_string()],
+                    mutable: true,
+                },
+                &[],
+                "Contract",
+                None,
+            )
+            .unwrap();
+
+        let resp: MemberListResponse = app
+            .wrap()
+            .query_wasm_smart(addr.clone(), &QueryMsg::Memberlist {})
+            .unwrap();
+        assert_eq!(
+            resp,
+            MemberListResponse {
+                members: vec![ALICE.to_string()],
+            }
+        );
+
+        let msg = ExecuteMsg::AddMembers { 
+            members: vec![BOB.to_string()],
+        };
+        let _ = app
+            .execute_contract(
+                Addr::unchecked("owner"),
+                addr.clone(),
+                &msg,
+                &[],
+            ).unwrap();
+        
+        let resp: MemberListResponse = app
+            .wrap()
+            .query_wasm_smart(addr.clone(), &QueryMsg::Memberlist {})
+            .unwrap();
+        assert_eq!(
+            resp,
+            MemberListResponse {
+                members: vec![ALICE.to_string(), BOB.to_string()],
+            }
+        );
+    }
+
+    #[test]
+    fn exec_remove_members() {
+        let mut app = App::default();
+
+        let code = ContractWrapper::new(execute, instantiate, query);
+        let code_id = app.store_code(Box::new(code));
+
+        let addr = app
+            .instantiate_contract(
+                code_id,
+                Addr::unchecked("owner"),
+                &InstantiateMsg {
+                    admins: vec![Addr::unchecked("owner").to_string()],
+                    members: vec![ALICE.to_string(), BOB.to_string()],
+                    mutable: true,
+                },
+                &[],
+                "Contract",
+                None,
+            )
+            .unwrap();
+
+        let resp: MemberListResponse = app
+            .wrap()
+            .query_wasm_smart(addr.clone(), &QueryMsg::Memberlist {})
+            .unwrap();
+        assert_eq!(
+            resp,
+            MemberListResponse {
+                members: vec![ALICE.to_string(), BOB.to_string()],
+            }
+        );
+
+        let msg = ExecuteMsg::RemoveMembers { 
+            members: vec![BOB.to_string()],
+        };
+        let _ = app
+            .execute_contract(
+                Addr::unchecked("owner"),
+                addr.clone(),
+                &msg,
+                &[],
+            ).unwrap();
+        
+        let resp: MemberListResponse = app
+            .wrap()
+            .query_wasm_smart(addr.clone(), &QueryMsg::Memberlist {})
+            .unwrap();
+        assert_eq!(
+            resp,
+            MemberListResponse {
+                members: vec![ALICE.to_string()],
             }
         );
     }
